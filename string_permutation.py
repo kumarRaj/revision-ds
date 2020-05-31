7dd8601f1b8aee93c4da92ff7ffd4d82acaf3be1
rkumar <rkumar@agoda.com>
2020-05-31 13:07:49 +0700

    String permutation all values

--- string_permutation.py
+++ string_permutation.py
@@ -1,19 +1,17 @@
-s = 'abcde'
+s = 'abc'
 
 def permute(s):
     q = []
     q.append(s[0])
     
-    current_index = 1
     while(len(q) != 0):
-        element = q.pop()
+        element = q.pop(0)
         if(len(element) == len(s)):
             break
+        current_index = len(element)
         for i in range(0, len(element) + 1):
             newPermutation = element[:i] + s[current_index] + element[i:]
             q.append(newPermutation)
-            print (newPermutation)
+    return q
 
-        current_index += 1
-
-permute(s)
+print(permute(s))
