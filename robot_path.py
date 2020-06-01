16d2cb87461f967e56c30cec8a865d27b021864f
rkumar <rkumar@agoda.com>
2020-06-01 22:49:45 +0700

    A working version

---
+++ robot_path.py
@@ -0,0 +1,41 @@
+grid = [[1,1], 
+        [1,1], 
+        [1,1]]
+
+class Robot:
+    def findPath(self, currentPosition, grid):
+        uniquePaths = 0
+        if self.canMoveRight(currentPosition, grid):
+            newPosition = self.moveRight(currentPosition)
+            if self.isGridEnd(newPosition, grid):
+                uniquePaths += 1
+            else: 
+                uniquePaths += self.findPath(newPosition, grid)
+
+        if self.canMoveDown(currentPosition, grid):
+            newPosition = self.moveDown(currentPosition)
+            if self.isGridEnd(newPosition, grid):
+                uniquePaths += 1
+            else: 
+                uniquePaths += self.findPath(newPosition, grid)
+
+        return uniquePaths
+
+    def isGridEnd(self, currentPosition, grid):
+        if not grid:
+            return True
+        x = len(grid) - 1
+        y = len(grid[0]) - 1
+        return currentPosition == (x,y)
+
+    def canMoveRight(self, currentPosition, grid):
+        return currentPosition[1] < len(grid[0]) - 1
+    def canMoveDown(self, currentPosition, grid):
+        return currentPosition[0] < len(grid) - 1
+    def moveRight(self, currentPosition):
+        return (currentPosition[0], currentPosition[1] + 1)
+    def moveDown(self, currentPosition):
+        return (currentPosition[0] + 1, currentPosition[1])
+
+
+print(Robot().findPath((0,0), grid))
